f6dfbb6173009611ea6f845c82b5b882cbc26f87
gonzarth <arthurgs2007@gmail.com>
2022-06-03 06:44:54 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a end-to-end machine learning project where I'm creading a regression model for ships' fuel consumption with a professional model serving rest api.',
+    author='Arthur G',
+    license='MIT',
+)

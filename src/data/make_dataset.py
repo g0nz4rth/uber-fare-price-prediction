33e9f083fb83ecb928f5372536c6007b4c7066e5
gonzarth <arthurgs2007@gmail.com>
2022-06-20 18:58:55 -0300

    project change update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,76 +1,31 @@
 # -*- coding: utf-8 -*-
-# import click
-# import logging
-# from pathlib import Path
-# from dotenv import find_dotenv, load_dotenv
-
-# custom dependencies
-import numpy as np
-import pandas as pd
-from sklearn import neighbors
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-
-seed = np.random.seed(42)
+import click
+import logging
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
 
 
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """ Runs data processing scripts to turn raw data from (../raw) into
-#         cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info('making final data set from raw data')
-
-
-def make_data_for_knn(data: pd.DataFrame, ignore: list, target: str):
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
     """
-    This function takes the original dataset and then makes it ready
-    for modeling with the k-NN algorithm.
-    """
-    # dropping useless columns
-    tmp_df = data.copy()
-    tmp_df = tmp_df.drop(ignore, axis=1)
-    
-    # dropping nans
-    tmp_df = tmp_df.dropna()
-    
-    # handling categorical data
-    tmp_df = pd.get_dummies(tmp_df)
-    
-    # splitting predictors-targets
-    predictors = tmp_df.drop([target], axis=1)
-    targets = tmp_df[target]
-    
-    # splitting the data into train and test sets
-    x_train, x_test, y_train, y_test = train_test_split(
-        predictors, 
-        targets, 
-        test_size = 0.3, 
-        shuffle=True,
-        random_state=seed
-    )
-    
-    # rescaling features
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    x_train = pd.DataFrame(scaler.fit_transform(x_train), columns=predictors.columns)
-    x_test = pd.DataFrame(scaler.fit_transform(x_test), columns=predictors.columns)
-    
-    return (x_train, x_test, y_train, y_test)
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
 
 
-# if __name__ == '__main__':
-#     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-#     # find .env automagically by walking up directories until it's found, then
-#     # load up the .env entries as environment variables
-#     load_dotenv(find_dotenv())
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
 
-#     main()
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+
+    main()
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,44 +1,4 @@
+# libs
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def na_impact_analysis(data: pd.DataFrame, column: str, figsize: tuple = (20, 6)):
-    """
-    This function implements a plot to analyze the impact of missing data
-    from a given column on the target variable.
-    """
-    # copying the original dataset
-    df = data.copy()
-    
-    # filtering missing values
-    df[column] = np.where(df[column].isnull(), 1, 0)
-    
-    # comparing the mean fuel consumption 
-    # for both missing and not missing groups
-    tmp = df.groupby(column)['FUEL_CONSUMPTION'].agg(['mean', 'std'])
-    
-    # making the plot
-    tmp.plot(
-        kind='barh', 
-        y='mean', 
-        legend=False,
-        xerr='std',
-        title='Fuel Consumption',
-        color='blue',
-        figsize=figsize
-    )
-    
-    plt.show()
-    
-def plot_monotonic_relationship(train_set, train_target, var, target):
-    """
-    This function plots the monotonic relationship that
-    exists between each encode categorical column with
-    the target column.
-    """
-    tmp_df = pd.concat([train_set, train_target], axis=1)
-    tmp_df.groupby(var)[target].median().plot.bar()
-    plt.title(var)
-    plt.ylim(2.2, 2.6)
-    plt.ylabel(target)
-    plt.show()
